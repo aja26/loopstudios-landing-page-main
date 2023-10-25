c523477ea620148f8c3a8d4eee0e8aefb5136d54
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-10-25 22:26:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const mobileMenuButton = document.getElementById('mobile-menu');
+const icon = mobileMenuButton.querySelector('img');
 const mobileMenu = document.getElementById('nav');
 
 
 mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('active')
-})
 
+    mobileMenu.classList.contains('active')
+        ? icon.src = "/images/icon-close.svg"
+        : icon.src ="/images/icon-hamburger.svg"
+    ;
+})
